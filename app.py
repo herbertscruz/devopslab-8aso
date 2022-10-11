b380413439206c282c80d2c4c130aefb4fcd5013
Herberts da Silva Cruz Fortuna <herbertscruz@gmail.com>
2022-10-10 21:02:20 -0300

    Update Message

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório DevOps - FIAP 8ASO"
+    return "Laboratório DevOps - FIAP 8ASO - Grupo 06"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
